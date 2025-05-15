c94585e6fd007a059f35da98bfac7ba9faab6d1c
Yuri-Bles <553227@student.fontys.nl>
2025-05-15 15:15:04 +0200

    Communities on Communities Page
    Communities from the database are now visible on the communitiespage.

---
+++ Pages/Communities.cshtml.cs
@@ -0,0 +1,30 @@
+using ClassLibraryLogicLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Alliance_Explorer.Pages
+{
+	public class CommunitiesModel : PageModel
+	{
+        public List<Community> Communities { get; set; } = new List<Community>();
+        public void Onget()
+        {
+	        bool useDatabase = true;
+
+	        if (useDatabase)
+	        {
+		        CommunityCollection communityCollectionDal = new CommunityCollection();
+		        Communities = communityCollectionDal.GetAllCommunities();
+			}
+	        else
+	        {
+		        Communities.Add(new Community("Subject 1", "Description 1"));
+		        Communities.Add(new Community("Subject 2", "Description 2"));
+		        Communities.Add(new Community("Subject 3", "Description 3"));
+		        Communities.Add(new Community("Subject 4", "Description 4"));
+		        Communities.Add(new Community("Subject 5", "Description 5"));
+		        Communities.Add(new Community("Subject 6", "Description 6"));
+			}
+        }
+    }
+}
