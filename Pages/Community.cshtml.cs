de36d57c5ee42c3ec54a1721f78eaf679924bd33
Yuri-Bles <553227@student.fontys.nl>
2025-06-14 15:19:48 +0200

    Allowed User to join Community

--- Pages/Community.cshtml.cs
+++ Pages/Community.cshtml.cs
@@ -1,5 +1,7 @@
 using ClassLibraryDAL;
 using ClassLibraryLogicLayer;
+using L3LogicLayer;
+using L5DAL;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -15,9 +17,15 @@ namespace Alliance_Explorer.Pages
 		[BindProperty(SupportsGet = true)]
 		public string? CommunitySubject { get; set; }
 
+		[BindProperty(SupportsGet = true)]
+		public bool Joined { get; set; } = false;
+
 		public Community? Community { get; set; } = null;
 
 		private CommunityCollection CommunityCollection = new CommunityCollection(new CommunityRepository());
+		private AccountCollection AccountCollection = new AccountCollection(new AccountRepository());
+
+		private Account currentAccount = null;
 
 		public List<Alliance> Alliances { get; set; } = new List<Alliance>();
         public List<Account> Admins { get; set; } = new List<Account>();
@@ -35,7 +43,20 @@ namespace Alliance_Explorer.Pages
 
 				this.Admins = this.Community.GetAdmins();
 				this.Members = this.Community.GetMembers();
+
+				this.currentAccount = AccountCollection.GetAccountByName(User.Identity.Name);
+				this.Joined = this.Community.IsAccountInCommunity(this.currentAccount);
 			}
+		}
+
+		public IActionResult OnPostJoin()
+		{
+			this.Community = CommunityCollection.FindCommunityByID(SelectedCommunityId.Value);
+			this.currentAccount = AccountCollection.GetAccountByName(User.Identity.Name);
+
+			this.Community.AccountJoinsCommunity(currentAccount);
+
+			return RedirectToPage(new { SelectedCommunityId = SelectedCommunityId });
 		}
 	}
 }
