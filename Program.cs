05c3bb6c7b71f3a930f58b1bde2b99b97103f082
Yuri-Bles <553227@student.fontys.nl>
2025-06-20 16:51:51 +0200

    DAL

--- Program.cs
+++ Program.cs
@@ -1,38 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddAuthentication("Cookies")
-	.AddCookie("Cookies", options =>
-	{
-		options.LoginPath = "/index"; // Redirect here if user is not logged in
-	});
-
-builder.Services.ConfigureApplicationCookie(options =>
-{
-	options.Cookie.HttpOnly = true;
-	options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-	options.Cookie.SameSite = SameSiteMode.Lax;
-});
-
-
-builder.Services.AddAuthorization();
-
 var app = builder.Build();
-
-app.UseAuthentication();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler(c => c.Run(async ctx =>
-	{
-		var ex = ctx.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerFeature>()?.Error;
-		Console.WriteLine(ex);
-		ctx.Response.Redirect("/Error");
-		await Task.CompletedTask;
-	}));
+	app.UseExceptionHandler("/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
 
